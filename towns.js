f7bfa7c1b1115d74396823451c45a02178e7c548
Nevena Firkova <nevena.firkova4@gmail.com>
2024-09-21 16:59:14 +0300

    Deleted unnecessary logic

--- towns.js
+++ towns.js
@@ -20,33 +20,9 @@ function deleteTown() {
         showMessage(townName + " not found.");
 }
 
-function addTown() {
-    let townName = $('#townNameForAdd').val();
-    $('#townNameForAdd').val('');
-    $('#towns').append($('<option>').text(townName));
-    $('#result').text(townName + " added.");
-}
-
 function showMessage(msg) {
     $('#result').text(msg).css("display", "block");
     setTimeout(function () {
         $('#result').hide('blind', {}, 500);
     }, 3000);
-}
-
-function shuffleTowns() {
-    let towns = $('#towns option').toArray();
-    $('#towns').empty();
-    shuffleArray(towns);
-    $('#towns').append(towns);
-    $('#result').text("Towns shuffled.");
-
-    function shuffleArray(array) {
-        for (var i = array.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var oldElement = array[i];
-            array[i] = array[j];
-            array[j] = oldElement;
-        }
-    }
 }
